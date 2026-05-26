5ed68ffab14ce2a2d4724129c561406b9c79d9a0
shokichi <2320024360@campus.ouj.ac.jp>
2026-05-26 09:31:20 +0900

    new file streamlit py1.py2.test.py

---
+++ 141_1_streamlit_start/py1.py
@@ -0,0 +1,12 @@
+import streamlit as st  # noqa
+
+"""
+# Streamlitをはじめよう
+
+Streamlitでは、Pythonのコードを記述するだけで、簡単にWebアプリケーションを作成できます。
+HTMLやCSSの知識がなくても、Pythonのコードを使ってインタラクティブなアプリケーションを構築できます。
+
+## PyQのリンク
+
+[PyQ](https://pyq.jp/)　｜　[PyQドキュメント](https://docs.pyq.jp/)　｜　[Python学習チャンネル](https://blog.pyq.jp/)
+"""
---
+++ 141_1_streamlit_start/test.py
@@ -0,0 +1,1 @@
+print("vs codeでpython実行できました。")
