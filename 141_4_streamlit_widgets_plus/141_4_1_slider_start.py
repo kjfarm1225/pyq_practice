3f48acf51adac8e63aa2a7626a5777ae905b9278
kjfarm1225 <2320024360@campus.ouj.ac.jp>
2026-06-02 15:09:07 +0900

    new file:   141_4_1_slider_start.py

---
+++ 141_4_streamlit_widgets_plus/141_4_1_slider_start.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+@st.cache
+def sample():
+    rng = np.random.default_rng()
+    return pd.DataFrame(rng.integers(0, 10, (6,3)), columns=["A", "B", "C"])
+
+df = sample()
+
+dc = {
+    "line": st.line_chart,
+    "bar": st.bar_chart,
+    "area": st.area_chart,
+}
+
+kind = st.sidebar.selectbox("kind", list(dc))
+
+sl = st.sidebar.slider("lower", 0, 4)
+
+dc[kind](df[sl:])
+
+if st.checkbox("DataFrame"):
+    st.dataframe(df[sl:].T)
